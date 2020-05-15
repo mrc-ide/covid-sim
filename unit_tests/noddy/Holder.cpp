9dc27ddec4356e794cd629cbb73918542c6f46db
Zebedee Mason <zeb@cyclerouter.co.uk>
2020-05-15 12:13:28 +0100

    Add in GoogleTest support and a first example that runs a test on build.

---
+++ unit_tests/noddy/Holder.cpp
@@ -0,0 +1,16 @@
+// Third party headers
+#include "gtest/gtest.h"
+
+namespace CovidSim
+{
+  namespace Test
+  {
+    namespace Noddy
+    {
+      TEST(Noddy, Holder)
+      {
+        EXPECT_TRUE(true);
+      }
+    }
+  }
+}
