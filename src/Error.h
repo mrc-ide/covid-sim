29788dc403747cd5344d55ab2a2ca9150fbd4984
Zlatan Vasović <zlatanvasovic@gmail.com>
2020-05-18 22:08:36 +0200

    Replace defines with const

--- src/Error.h
+++ src/Error.h
@@ -9,7 +9,7 @@ __declspec(noreturn)
 #endif
 void ErrorCritical(const char* msg, const char* file, int line, ...)
 #ifdef __GNUC__
-                                            __attribute__ ((noreturn)) __attribute__ ((format (printf, 1, 4)))
+  __attribute__ ((noreturn)) __attribute__ ((format (printf, 1, 4)))
 #endif
 ;
 
