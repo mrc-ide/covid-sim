b3049a03e236ec49c6f80ec9047baa4823ac4e4e
Nicholas Rodine <halfofastaple@gmail.com>
2020-05-20 17:05:53 -0500

    Added MicroCellPosition class.

---
+++ src/MicroCellPosition.cpp
@@ -0,0 +1,6 @@
+#include "MicroCellPosition.hpp"
+
+MicroCellPosition::MicroCellPosition(int x, int y)
+	:x(x),
+	y(y)
+{}
---
+++ src/MicroCellPosition.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class MicroCellPosition {
+public:
+	const int x;
+	const int y;
+
+	MicroCellPosition(int x, int y);
+};
+
