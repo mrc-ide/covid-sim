07157442da868f73c6901967eb501a0b3eabaf8c
Zlatan Vasović <zlatanvasovic@gmail.com>
2020-05-20 11:41:25 +0200

    stdlib.h -> cstdlib

--- src/CovidSim.h
+++ src/CovidSim.h
@@ -5,7 +5,7 @@
 
 #include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <stddef.h>
 #include <math.h>
 #include <string.h>
--- src/Dist.cpp
+++ src/Dist.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <cstdlib>
 #include <math.h>
 
 #include "Constants.h"
