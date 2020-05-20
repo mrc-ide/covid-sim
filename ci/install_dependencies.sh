a3d5ba6123d15069407faa38a66d5bfc3f858c3a
Zebedee Mason <zeb@cyclerouter.co.uk>
2020-05-20 10:08:39 +0100

    Make GoogleTest safe for CI

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -10,38 +10,38 @@ distro_id=$(grep '^ID=' /etc/os-release|awk -F = '{print $2}'|sed 's/\"//g')
 
 case "$distro_id" in
     'fedora')
-        dnf -y --refresh install cmake make g++ diffutils wget git
+        dnf -y --refresh install cmake make g++ diffutils #wget git
         ;;
 
     'debian')
         apt-get update
-        apt-get install -y cmake make g++ python3 wget git
+        apt-get install -y cmake make g++ python3 #wget git
         ;;
 
     'arch')
         pacman --noconfirm -Syu
-        pacman --noconfirm -S cmake make gcc diffutils python3 wget git
+        pacman --noconfirm -S cmake make gcc diffutils python3 #wget git
         ;;
 
     'ubuntu')
         apt-get update
-        apt-get install -y cmake make g++ python3 doxygen wget
+        apt-get install -y cmake make g++ python3 doxygen #wget
         ;;
 
     'centos'|'rhel')
         yum -y install epel-release
-        yum -y install cmake3 make gcc-c++ python3 wget git
+        yum -y install cmake3 make gcc-c++ python3 #wget git
         ln -s /bin/cmake3 /bin/cmake
         ;;
 
     'opensuse'|'opensuse-tumbleweed')
         zypper --non-interactive refresh
-        zypper --non-interactive install cmake make gcc-c++ wget tar git
+        zypper --non-interactive install cmake make gcc-c++ #wget tar git
         ;;
 
     'alpine')
         apk update
-        apk add cmake make g++ bash python3 coreutils git
+        apk add cmake make g++ bash python3 coreutils #git
         ;;
     *)
         echo "Unsupported distribution: $distro_id"
@@ -50,6 +50,6 @@ case "$distro_id" in
 esac
 
 # Download CMake and add its location to the path
-wget https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.tar.gz
-tar xf cmake-3.17.2-Linux-x86_64.tar.gz
-export PATH="`pwd`/cmake-3.17.2-Linux-x86_64/bin:$PATH"
+#wget https://cmake.org/files/v3.17/cmake-3.17.2-Linux-x86_64.tar.gz
+#tar xf cmake-3.17.2-Linux-x86_64.tar.gz
+#export PATH="`pwd`/cmake-3.17.2-Linux-x86_64/bin:$PATH"
