d3001087780f3f65d6170de3b9469bdc37b29d72
Nicholas Rodine <halfofastaple@gmail.com>
2020-05-21 15:42:54 -0500

    Added distance_to and distance_squared_to functions for location.

--- src/Location.cpp
+++ src/Location.cpp
@@ -1,1 +1,12 @@
+#include <cmath>
+
 #include "Location.hpp"
+#include "Dist.h"
+
+float Location::distance_to(Location other) const {
+	return std::sqrt(this->distance_squared_to(other));
+}
+
+float Location::distance_squared_to(Location other) const {
+	return dist2_raw(this->x, this->y, other.x, other.y);
+}
--- src/Location.hpp
+++ src/Location.hpp
@@ -3,5 +3,8 @@
 struct Location {
 	const float x;
 	const float y;
+
+	float distance_to(Location other) const;
+	float distance_squared_to(Location other) const;
 };
 
