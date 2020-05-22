89acc129ee4467820655e5224a5ba37af604039f
Nicholas Rodine <halfofastaple@gmail.com>
2020-05-22 13:26:00 -0500

    Moved Microcell and IndexList out of Model.h

---
+++ src/Microcell.cpp
@@ -0,0 +1,1 @@
+#include "Microcell.h"
--- src/Model.h
+++ src/Model.h
@@ -8,6 +8,7 @@
 #include "Coordinates/Location.h"
 #include "Household.h"
 #include "Cell.h"
+#include "Microcell.h"
 
 //// need to test that inequalities in IncubRecoverySweep can be replaced if you initialize to USHRT_MAX, rather than zero.
 //// need to output quantities by admin unit
@@ -226,15 +227,6 @@ struct Events
 */
 
 /**
- * @brief Used for computing spatial interactions more efficiently.
- */
-struct IndexList
-{
-	int id;
-	float prob;
-};
-
-/**
  * @brief Airport state.
  *
  * Not used for COVID-19 right now. Might be more relevant for USA and
@@ -248,33 +240,6 @@ struct Airport
 	Location loc;
 	float* prop_traffic;
 	IndexList* DestMcells, *DestPlaces;
-};
-
-/**
- * @brief The basic unit of the simulation and is associated to a geographical location.
- *
- * Interventions (e.g., school closures) are tracked at this level. It contains a list of its
- * members (people), places (schools, universities, workplaces etc.), road networks, links to
- * airports etc.
- */
-struct Microcell
-{
-	/* Note use of short int here limits max run time to USHRT_MAX*TimeStep - e.g. 65536*0.25=16384 days=44 yrs.
-	   Global search and replace of 'unsigned short int' with 'int' would remove this limit, but use more memory.
-	*/
-	int n /*Number of people in microcell*/, adunit;
-	int* members;
-	unsigned short int country;
-
-	int* places[NUM_PLACE_TYPES];
-	unsigned short int np[NUM_PLACE_TYPES];
-	unsigned short int moverest, placeclose, socdist, keyworkerproph, move_trig, place_trig, socdist_trig, keyworkerproph_trig;
-	unsigned short int move_start_time, move_end_time;
-	unsigned short int place_end_time, socdist_end_time, keyworkerproph_end_time;
-	unsigned short int treat, vacc, treat_trig, vacc_trig;
-	unsigned short int treat_start_time, treat_end_time;
-	unsigned short int vacc_start_time;
-	IndexList* AirportList;
 };
 
 /**
