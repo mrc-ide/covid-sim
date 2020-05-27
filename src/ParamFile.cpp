859a03102b3eafa7ebd003d27a060f55884086b3
Ozmorph <ozmorph@gmail.com>
2020-05-27 15:09:34 -0400

    replaced std::not1() macro with lambda for clarity; replaced 'default' argument name with 'default_value' to not use reserved keyword

--- src/ParamFile.cpp
+++ src/ParamFile.cpp
@@ -9,19 +9,19 @@
 
 #include "ParamFile.hpp"
 
-// ltrim(), rtrim(), and trim() are from a highly upvoted answer on StackOverflow
+// ltrim(), rtrim(), and trim() are from a highly up voted answer on StackOverflow
 // https://stackoverflow.com/a/217605
 
 // trim from start (in place)
 static inline void ltrim(std::string& s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
+            [](int ch) { return !std::isspace(ch); }));
 }
 
 // trim from end (in place)
 static inline void rtrim(std::string& s) {
     s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+            [](int ch) { return !std::isspace(ch); }).base(), s.end());
 }
 
 // trim from both ends (in place)
