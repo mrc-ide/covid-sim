4f0dd99835529a01eb6dfae4cf52743d413c8913
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-05-30 16:03:21 +0100

    Lib import for clang

--- src/CovidSim.h
+++ src/CovidSim.h
@@ -1,6 +1,15 @@
 #ifndef COVIDSIM_COVIDSIM_H_INCLUDED_
 #define COVIDSIM_COVIDSIM_H_INCLUDED_
 
+// Compiling with Clang from VS requires libomp.lib to be included in
+// Project, Properties, Input, Additional Dependencies, however
+// that causes VS/Intel compilers to fail to build if libomp.lib is
+// not found. The following should include the lib file only on Clang.
+
+#ifdef __clang__
+#pragma comment(lib, "libomp.lib")
+#endif
+
 #include "MachineDefines.h"
 
 #include <limits.h>
