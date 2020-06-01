e8c1762ff03ebdd18d77d997e59f6ac49758d1f1
NeilFerguson <neil.ferguson@imperial.ac.uk>
2020-06-01 09:29:06 +0100

    Add files to allow Report 9 tables to be reproduced.

---
+++ report9/GB_suppress/runonce.ps1
@@ -0,0 +1,3 @@
+
+.\CovidSim.exe /c:8 /PP:preGB_R0=2.0.txt /P:p_NoInt.txt /CLP1:100000 /CLP2:0 /O:NoInt_R0=2.4 /D:..\population\GB_pop2018_nhs.txt /M:..\population\GB_pop2018.bin /S:..\population\NetworkGB_8T.bin /R:1.2 98798150 729101 17389101 4797132
+
