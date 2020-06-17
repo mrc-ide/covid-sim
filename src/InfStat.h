abda2bc9b7de6b31f56f68b952ad1f1fbe738f3c
Zlatan Vasović <zlatanvasovic@gmail.com>
2020-06-17 17:58:14 +0200

    Improve InfStat comments [ci skip]

--- src/InfStat.h
+++ src/InfStat.h
@@ -1,23 +1,23 @@
 #ifndef COVIDSIM_INFSTAT_H_INCLUDED_
 #define COVIDSIM_INFSTAT_H_INCLUDED_
 
-//// Infection Status definitions / labels (generally positive value indicates asymptomatic infection, 
+//// Infection Status definitions / labels (generally positive value indicates asymptomatic infection,
 //// negative value indicates symptomatic infection).
 
-enum InfStat {	
-  
-//// Note - DO NOT CHANGE these definitions without accounting for "Quarantined not Infected" / 
+enum InfStat {
+
+//// Note - DO NOT CHANGE these definitions without accounting for "Quarantined not Infected" /
 //// "Quarantined not symptomatic" calculation: relies on value below being negative for symptomatic people.
 
 	//// Susceptible
 	InfStat_Susceptible = 0,
-	//// E or L (neither infectious nor symptomatic).
+	//// Neither infectious nor symptomatic (E or L).
 	InfStat_Latent = 1,
 	//// Infectious and about to become a case.
 	InfStat_InfectiousAlmostSymptomatic = -1,
-	//// Not just asymptomatic, but also will not become symptomatic (i.e. a case.)
+	//// Not just asymptomatic, but also will not become symptomatic (i.e. a case)
 	InfStat_InfectiousAsymptomaticNotCase = 2,
-	//// case. Infectious and symptomatic.
+	//// Case. Infectious and symptomatic.
 	InfStat_Case = -2,
 	//// Recovered from asymptomatic infection
 	InfStat_RecoveredFromAsymp = 3,
@@ -25,7 +25,7 @@ enum InfStat {
 	InfStat_RecoveredFromSymp = -3,
 	//// InfStat_Recovered (will use this for abs() values) so code reads correctly
 	InfStat_Recovered = 3,
-	//// Immune at start of epidemic - used to model partially immune population. Distinct therefore from recovered, who recovered during runtime. Doesn't take negative values.
+	//// Immune at start of epidemic - used to model partially immune population. Distinct from recovered, who recovered during runtime. Doesn't take negative values.
 	InfStat_ImmuneAtStart = 4,
 	//// Dead was asymptomatic
 	InfStat_Dead_WasAsymp = 5,
@@ -45,9 +45,9 @@ enum Severity {
 	Severity_Critical = 3,
 	//// Recovering from Critical (not recovered yet).
 	Severity_RecoveringFromCritical = 4,
-	//// label to avoid double counting. Not sure you need.
+	//// Label to avoid double counting. Not sure you need.
 	Severity_Dead = 5,
-	//// label to avoid double counting. Not sure you need.
+	//// Label to avoid double counting. Not sure you need.
 	Severity_Recovered = 6
 };
 
