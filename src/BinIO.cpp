3b4fec5919e1820ba666a15e2ac611852bd71295
Zlatan Vasović <zlatanvasovic@gmail.com>
2020-06-18 17:35:22 +0200

    Abstract fread/write_big into f_big

--- src/BinIO.cpp
+++ src/BinIO.cpp
@@ -1,40 +1,37 @@
 #include <cstdio>
 #include "BinIO.h"
 
-size_t fwrite_big(void *buffer,size_t size,size_t count, FILE *stream)
+size_t f_big(void *buffer, size_t size, size_t count, FILE *stream, int ftype)
 {
-	const unsigned long long mx=0x80000000;
-	unsigned long long j,n,st;
-	size_t ret=0;
+	const unsigned long long mx = 0x80000000;
+	unsigned long long j, n, st;
+	size_t ret = 0;
 	char *buf2;
 
-	st=mx/((unsigned long long) size);
-	n=count/st;
-	for(j=0;j<n;j++)
-		{
-		buf2=((char *)buffer)+j*st*size;
-		ret+=(fwrite(buf2,size,(size_t) st,stream));
-		}
-	buf2=((char *)buffer)+n*st*size;
-	ret+=(fwrite(buf2,size,(size_t) (count-n*st),stream));
+	st = mx / ((unsigned long long) size);
+	n = count / st;
+	for (j = 0; j < n; j++)
+	{
+		buf2 = ((char *) buffer) + j*st*size;
+		if (ftype == 0)
+			ret += (fwrite(buf2, size, (size_t) st, stream));
+		else if (ftype == 1)
+			ret += (fread(buf2, size, (size_t) st, stream));
+	}
+	buf2 = ((char *) buffer) + n*st*size;
+	if (ftype == 0)
+		ret += (fwrite(buf2, size, (size_t) (count - n*st), stream));
+	else if (ftype == 1)
+		ret += (fread(buf2, size, (size_t) (count - n*st), stream));
 	return ret;
 }
 
-size_t fread_big(void *buffer,size_t size,size_t count, FILE *stream)
+size_t fwrite_big(void *buffer, size_t size, size_t count, FILE *stream)
 {
-	const unsigned long long mx=0x80000000;
-	unsigned long long j,n,st;
-	size_t ret=0;
-	char *buf2;
+	return f_big(buffer, size, count, stream, 0);
+}
 
-	st=mx/((unsigned long long) size);
-	n=count/st;
-	for(j=0;j<n;j++)
-		{
-		buf2=((char *)buffer)+j*st*size;
-		ret+=(fread(buf2,size,(size_t) st,stream));
-		}
-	buf2=((char *)buffer)+n*st*size;
-	ret+=(fread(buf2,size,(size_t) (count-n*st),stream));
-	return ret;
+size_t fread_big(void *buffer, size_t size, size_t count, FILE *stream)
+{
+	return f_big(buffer, size, count, stream, 1);
 }
