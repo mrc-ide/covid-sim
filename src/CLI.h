3003cbea2cc0f1f0b8f72b29259770865c20076d
Ozmorph <ozmorph@gmail.com>
2020-06-22 17:42:11 -0400

    convert spaces to tabs in CLI.h/.cpp

--- src/CLI.h
+++ src/CLI.h
@@ -34,61 +34,61 @@ void parse_double(std::string const& input, double& output);
 
 class CmdLineArgs {
 public:
-    // Function prototype for a generic parser function
-    using ParserFn = std::function<void(std::string const&)>;
-    // Function prototype for a string parser function
-    using StringParserFn = std::function<void(std::string const&, std::string&)>;
+	// Function prototype for a generic parser function
+	using ParserFn = std::function<void(std::string const&)>;
+	// Function prototype for a string parser function
+	using StringParserFn = std::function<void(std::string const&, std::string&)>;
 
-    /**
-     * Use this function when adding a new option to the CLI that needs a custom
-     * callback function (e.g. it does more than read a string or integer).
-     *
-     * This will insert the custom callback function into a map of other options.
-     */
-    void add_custom_option(std::string const& option, ParserFn func, std::string const& doc);
+	/**
+	 * Use this function when adding a new option to the CLI that needs a custom
+	 * callback function (e.g. it does more than read a string or integer).
+	 *
+	 * This will insert the custom callback function into a map of other options.
+	 */
+	void add_custom_option(std::string const& option, ParserFn func, std::string const& doc);
 
-    /**
-     * Use this function when adding a new double option to the CLI.
-     *
-     * This will bind the output variable to the parse_double() function and
-     * gets inserted into a map of other options. This provides a strong cohesion
-     * between an option name (i.e. 'R') with its variable (i.e. 'P.R0scale')
-     */
-    void add_double_option(std::string const& option, double& output, std::string const& doc);
+	/**
+	 * Use this function when adding a new double option to the CLI.
+	 *
+	 * This will bind the output variable to the parse_double() function and
+	 * gets inserted into a map of other options. This provides a strong cohesion
+	 * between an option name (i.e. 'R') with its variable (i.e. 'P.R0scale')
+	 */
+	void add_double_option(std::string const& option, double& output, std::string const& doc);
 
-    /**
-     * Use this function when adding a new integral option to the CLI.
-     *
-     * This will bind the output variable to the parse_integer() function and
-     * gets inserted into a map of other options. This provides a strong cohesion
-     * between an option name (i.e. 'c') with its variable (i.e. 'P.MaxNumThreads')
-     */
-    void add_integer_option(std::string const& option, int& output, std::string const& doc);
+	/**
+	 * Use this function when adding a new integral option to the CLI.
+	 *
+	 * This will bind the output variable to the parse_integer() function and
+	 * gets inserted into a map of other options. This provides a strong cohesion
+	 * between an option name (i.e. 'c') with its variable (i.e. 'P.MaxNumThreads')
+	 */
+	void add_integer_option(std::string const& option, int& output, std::string const& doc);
 
-    /**
-     * Use this function when adding a new string option to the CLI.
-     *
-     * This will bind the output variable specified to a parser function which
-     * then gets inserted into a map of other options. This provides a strong
-     * cohesion between an option name (i.e. 'P') with a C++ variable (i.e. 'ParamFile')
-     */
-    void add_string_option(std::string const& option, StringParserFn func, std::string& output, std::string const& doc);
+	/**
+	 * Use this function when adding a new string option to the CLI.
+	 *
+	 * This will bind the output variable specified to a parser function which
+	 * then gets inserted into a map of other options. This provides a strong
+	 * cohesion between an option name (i.e. 'P') with a C++ variable (i.e. 'ParamFile')
+	 */
+	void add_string_option(std::string const& option, StringParserFn func, std::string& output, std::string const& doc);
 
-    /**
-     * Call this function once all add_option() calls have been made to process
-     * the arguments passed in from the command-line.
-     */
-    void parse(int argc, char* argv[], Param& P);
+	/**
+	 * Call this function once all add_option() calls have been made to process
+	 * the arguments passed in from the command-line.
+	 */
+	void parse(int argc, char* argv[], Param& P);
 
-    void print_help();
-    void print_help_and_exit();
+	void print_help();
+	void print_help_and_exit();
 
-    void print_detailed_help();
-    void print_detailed_help_and_exit();
+	void print_detailed_help();
+	void print_detailed_help_and_exit();
 
 private:
-    std::map<std::string, ParserFn> option_map_;
-    std::map<std::string, std::string> doc_map_;
+	std::map<std::string, ParserFn> option_map_;
+	std::map<std::string, std::string> doc_map_;
 };
 
 #endif
