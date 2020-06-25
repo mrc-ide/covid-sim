ea744d4a2725877e1f74cd1b20f26a58c3abf380
Ozmorph <ozmorph@gmail.com>
2020-06-25 12:32:20 -0400

    removed inline keyword from string trim functions in ParamFile.cpp

--- src/ParamFile.cpp
+++ src/ParamFile.cpp
@@ -14,19 +14,19 @@
 // https://stackoverflow.com/a/217605
 
 // trim from start (in place)
-static inline void ltrim(std::string& s) {
+static void ltrim(std::string& s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
             [](int ch) { return !std::isspace(ch); }));
 }
 
 // trim from end (in place)
-static inline void rtrim(std::string& s) {
+static void rtrim(std::string& s) {
     s.erase(std::find_if(s.rbegin(), s.rend(),
             [](int ch) { return !std::isspace(ch); }).base(), s.end());
 }
 
 // trim from both ends (in place)
-static inline void trim(std::string& s) {
+static void trim(std::string& s) {
     ltrim(s);
     rtrim(s);
 }
