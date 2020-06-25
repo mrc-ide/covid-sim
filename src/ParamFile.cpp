4787c28da54feab3ebc339b8988e0b004c6953fe
Ozmorph <ozmorph@gmail.com>
2020-06-25 12:34:56 -0400

    converted ParamFile.cpp and ParamFile.h to use tabs instead of spaces

--- src/ParamFile.cpp
+++ src/ParamFile.cpp
@@ -15,181 +15,181 @@
 
 // trim from start (in place)
 static void ltrim(std::string& s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            [](int ch) { return !std::isspace(ch); }));
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+			[](int ch) { return !std::isspace(ch); }));
 }
 
 // trim from end (in place)
 static void rtrim(std::string& s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            [](int ch) { return !std::isspace(ch); }).base(), s.end());
+	s.erase(std::find_if(s.rbegin(), s.rend(),
+			[](int ch) { return !std::isspace(ch); }).base(), s.end());
 }
 
 // trim from both ends (in place)
 static void trim(std::string& s) {
-    ltrim(s);
-    rtrim(s);
+	ltrim(s);
+	rtrim(s);
 }
 
 ParamReader::ParamReader(std::string const& param_file, std::string const& preparam_file, std::string const& admin_file)
 {
-    // duplicate params from different files take priority over one another:
-    //     admin_file >> preparam_file >> param_file
-    parse_param_file(param_file);
-    parse_param_file(preparam_file);
-    parse_param_file(admin_file);
+	// duplicate params from different files take priority over one another:
+	//	 admin_file >> preparam_file >> param_file
+	parse_param_file(param_file);
+	parse_param_file(preparam_file);
+	parse_param_file(admin_file);
 }
 
 template<typename T>
 bool ParamReader::raw_extract(std::istringstream& stream, T& output)
 {
-    static_assert(std::is_integral<T>::value || std::is_floating_point<T>::value,
-                  "Integral or floating point required.");
+	static_assert(std::is_integral<T>::value || std::is_floating_point<T>::value,
+				  "Integral or floating point required.");
 
-    stream >> output;
-    if (stream.fail()) {
-        if (output == std::numeric_limits<T>::max()) {
-            std::cerr << "OVERFLOW: detected a number larger than " << std::numeric_limits<T>::max()
-                      << " when parsing " << stream.str() << std::endl;
-        }
-        else if (output == std::numeric_limits<T>::min()) {
-            std::cerr << "UNDERFLOW: detected a number smaller than " << std::numeric_limits<T>::min()
-                      << " when parsing " << stream.str() << std::endl;
-        }
-        else {
-            std::cerr << "ERROR: Expected integral type, got " << stream.str() << std::endl;
-        }
-        return false;
-    }
-    return true;
+	stream >> output;
+	if (stream.fail()) {
+		if (output == std::numeric_limits<T>::max()) {
+			std::cerr << "OVERFLOW: detected a number larger than " << std::numeric_limits<T>::max()
+					  << " when parsing " << stream.str() << std::endl;
+		}
+		else if (output == std::numeric_limits<T>::min()) {
+			std::cerr << "UNDERFLOW: detected a number smaller than " << std::numeric_limits<T>::min()
+					  << " when parsing " << stream.str() << std::endl;
+		}
+		else {
+			std::cerr << "ERROR: Expected integral type, got " << stream.str() << std::endl;
+		}
+		return false;
+	}
+	return true;
 }
 
 template<typename T>
 bool ParamReader::extract(std::string const& param, T& output, T default_value)
 {
-    if (!exists(param))
-    {
-        std::cout << "Using default value: " << default_value << std::endl;
-        output = default_value;
-        return false;
-    }
+	if (!exists(param))
+	{
+		std::cout << "Using default value: " << default_value << std::endl;
+		output = default_value;
+		return false;
+	}
 
-    std::istringstream iss(m_param_value_map[param]);
-    return raw_extract(iss, output);
+	std::istringstream iss(m_param_value_map[param]);
+	return raw_extract(iss, output);
 }
 
 template<typename T>
 void ParamReader::extract_or_exit(std::string const& param, T& output)
 {
-    if (!exists(param))
-        std::exit(1);
+	if (!exists(param))
+		std::exit(1);
 
-    std::istringstream iss(m_param_value_map[param]);
-    if (!raw_extract(iss, output))
-        std::exit(1);
+	std::istringstream iss(m_param_value_map[param]);
+	if (!raw_extract(iss, output))
+		std::exit(1);
 }
 
 template<typename T>
 bool ParamReader::extract_multiple_no_default(std::string const& param, T* output, std::size_t N)
 {
-    if (!exists(param))
-        return false;
+	if (!exists(param))
+		return false;
 
-    std::istringstream iss(m_param_value_map[param]);
-    for (auto i = 0; i < N; i++)
-    {
-        if (!raw_extract(iss, output[i]))
-        {
-            std::cerr << "ERROR: Got " << i << " out of " << N << " parameters for "
-                      << param << std::endl;
-            return false;
-        }
-    }
-    return true;
+	std::istringstream iss(m_param_value_map[param]);
+	for (auto i = 0; i < N; i++)
+	{
+		if (!raw_extract(iss, output[i]))
+		{
+			std::cerr << "ERROR: Got " << i << " out of " << N << " parameters for "
+					  << param << std::endl;
+			return false;
+		}
+	}
+	return true;
 }
 
 template<typename T>
 void ParamReader::extract_multiple_or_exit(std::string const& param, T* output, std::size_t N)
 {
-    if (extract_multiple_no_default(param, output, N) == false)
-        std::exit(1);
+	if (extract_multiple_no_default(param, output, N) == false)
+		std::exit(1);
 }
 
 template<typename T>
 bool ParamReader::extract_multiple(std::string const& param, T* output, std::size_t N, T default_value)
 {
-    if (extract_multiple_no_default(param, output, N) == false)
-    {
-        std::cerr << "Using default value: " << default_value << std::endl;
-        std::fill_n(output, N, default_value);
-        return false;
-    }
-    return true;
+	if (extract_multiple_no_default(param, output, N) == false)
+	{
+		std::cerr << "Using default value: " << default_value << std::endl;
+		std::fill_n(output, N, default_value);
+		return false;
+	}
+	return true;
 }
 
 template<typename T>
 bool ParamReader::cond_extract_multiple(bool conditional, std::string const& param, T* output, std::size_t N, T default_value)
 {
-    if (conditional)
-        return extract_multiple(param, output, N, default_value);
-    std::fill_n(output, N, default_value);
-    return true;
+	if (conditional)
+		return extract_multiple(param, output, N, default_value);
+	std::fill_n(output, N, default_value);
+	return true;
 }
 
 bool ParamReader::extract_multiple_strings_no_default(std::string const& param, std::vector<std::string>& output, std::size_t N)
 {
-    if (!exists(param))
-        return false;
+	if (!exists(param))
+		return false;
 
-    std::istringstream iss(m_param_value_map[param]);
-    std::string token;
-    for (auto i = 0; i < N; i++)
-    {
-        iss >> token;
-        if (iss.fail())
-        {
-            std::cerr << "ERROR: Only got " << i << " out of " << N << " parameters for " << param << std::endl;
-            return false;
-        }
-        output.push_back(std::move(token));
-    }
-    return true;
+	std::istringstream iss(m_param_value_map[param]);
+	std::string token;
+	for (auto i = 0; i < N; i++)
+	{
+		iss >> token;
+		if (iss.fail())
+		{
+			std::cerr << "ERROR: Only got " << i << " out of " << N << " parameters for " << param << std::endl;
+			return false;
+		}
+		output.push_back(std::move(token));
+	}
+	return true;
 }
 
 void ParamReader::extract_multiple_strings_or_exit(std::string const& param, std::vector<std::string>& output, std::size_t N)
 {
-    if (!extract_multiple_strings_no_default(param, output, N))
-        std::exit(1);
+	if (!extract_multiple_strings_no_default(param, output, N))
+		std::exit(1);
 }
 
 void ParamReader::extract_string_matrix_or_exit(std::string const& param, std::vector<std::vector<std::string>>& output, std::size_t num_cols)
 {
-    if (!exists(param))
-        std::exit(1);
+	if (!exists(param))
+		std::exit(1);
 
-    // iterate over every row of the matrix
-    std::istringstream matrix(m_param_value_map[param]);
-    std::istringstream line_stream;
-    for (std::string line; std::getline(matrix, line);)
-    {
-        // iterate over every token in each row and add it in a vector
-        line_stream.str(line);
-        line_stream.clear();
-        std::vector<std::string> new_row;
-        for (auto i = 0; i < num_cols; i++)
-        {
-            std::string token;
-            line_stream >> token;
-            if (line_stream.fail())
-            {
-                std::cerr << "ERROR: Only got " << i << " out of " << num_cols << " columns in line (" << line
-                          << ") for parameter " << param << std::endl;
-                std::exit(1);
-            }
-            new_row.push_back(std::move(token));
-        }
-        output.push_back(std::move(new_row));
-    }
+	// iterate over every row of the matrix
+	std::istringstream matrix(m_param_value_map[param]);
+	std::istringstream line_stream;
+	for (std::string line; std::getline(matrix, line);)
+	{
+		// iterate over every token in each row and add it in a vector
+		line_stream.str(line);
+		line_stream.clear();
+		std::vector<std::string> new_row;
+		for (auto i = 0; i < num_cols; i++)
+		{
+			std::string token;
+			line_stream >> token;
+			if (line_stream.fail())
+			{
+				std::cerr << "ERROR: Only got " << i << " out of " << num_cols << " columns in line (" << line
+						  << ") for parameter " << param << std::endl;
+				std::exit(1);
+			}
+			new_row.push_back(std::move(token));
+		}
+		output.push_back(std::move(new_row));
+	}
 }
 
 // Explicit template instantiations for the linker
@@ -209,72 +209,72 @@ template void ParamReader::extract_multiple_or_exit<int>(std::string const&, int
 
 bool ParamReader::exists(std::string const& param)
 {
-    auto it = m_param_value_map.find(param);
-    if (it == m_param_value_map.cend())
-    {
-        std::cout << "ERROR: Parameter \"" << param << "\" not found." << std::endl;
-        return false;
-    }
-    return true;
+	auto it = m_param_value_map.find(param);
+	if (it == m_param_value_map.cend())
+	{
+		std::cout << "ERROR: Parameter \"" << param << "\" not found." << std::endl;
+		return false;
+	}
+	return true;
 }
 
 void ParamReader::parse_param_file(std::string const& param_file)
 {
-    std::ifstream param_stream(param_file);
-    if (!param_stream)
-    {
-        std::cerr << "Could not open the specified param file: " << param_file << std::endl;
-        return;
-    }
+	std::ifstream param_stream(param_file);
+	if (!param_stream)
+	{
+		std::cerr << "Could not open the specified param file: " << param_file << std::endl;
+		return;
+	}
 
-    std::string param_line;
-    while (std::getline(param_stream, param_line))
-    {
-        // strip all whitespace characters from the line
-        trim(param_line);
+	std::string param_line;
+	while (std::getline(param_stream, param_line))
+	{
+		// strip all whitespace characters from the line
+		trim(param_line);
 
-        // skip empty lines
-        if (param_line.empty())
-            continue;
+		// skip empty lines
+		if (param_line.empty())
+			continue;
 
-        // check if this line is a parameter
-        if (param_line.front() == '[' && param_line.back() == ']') {
-            // remove the brackets
-            param_line.erase(param_line.begin());
-            param_line.pop_back();
-            // try to read the value of this parameter into a string
-            std::string value_line;
-            std::string tmp_line;
-            // read every line after the parameter name until there's no more or a break
-            while (std::getline(param_stream, tmp_line))
-            {
-                trim(tmp_line);
-                // break if the next line is empty or doesn't start with a letter, number, or '-' character
-                if (tmp_line.empty() || !(std::isalnum(tmp_line[0]) || tmp_line[0] == '-'))
-                    break;
-                if (!value_line.empty())
-                    value_line.push_back('\n');
-                value_line.append(tmp_line);
-            }
-            // if there is no value found for the parameter, print an error and continue
-            if (value_line.empty())
-            {
-                std::cerr << "ERROR: Value is missing for parameter: " << param_line << std::endl;
-                continue;
-            }
+		// check if this line is a parameter
+		if (param_line.front() == '[' && param_line.back() == ']') {
+			// remove the brackets
+			param_line.erase(param_line.begin());
+			param_line.pop_back();
+			// try to read the value of this parameter into a string
+			std::string value_line;
+			std::string tmp_line;
+			// read every line after the parameter name until there's no more or a break
+			while (std::getline(param_stream, tmp_line))
+			{
+				trim(tmp_line);
+				// break if the next line is empty or doesn't start with a letter, number, or '-' character
+				if (tmp_line.empty() || !(std::isalnum(tmp_line[0]) || tmp_line[0] == '-'))
+					break;
+				if (!value_line.empty())
+					value_line.push_back('\n');
+				value_line.append(tmp_line);
+			}
+			// if there is no value found for the parameter, print an error and continue
+			if (value_line.empty())
+			{
+				std::cerr << "ERROR: Value is missing for parameter: " << param_line << std::endl;
+				continue;
+			}
 
-            // store the parameter name and value for later use by ReadParams()
-            auto it = m_param_value_map.find(param_line);
-            if (it != m_param_value_map.cend())
-            {
-                std::cerr << "OVERRIDE: Param file (\"" << param_file << "\") is updating Key:["
-                          << param_line << "]" << " Value:[" << value_line << "]" << std::endl;
-                it->second = value_line;
-            }
-            else
-            {
-                m_param_value_map.emplace(param_line, value_line);
-            }
-        }
-    }
+			// store the parameter name and value for later use by ReadParams()
+			auto it = m_param_value_map.find(param_line);
+			if (it != m_param_value_map.cend())
+			{
+				std::cerr << "OVERRIDE: Param file (\"" << param_file << "\") is updating Key:["
+						  << param_line << "]" << " Value:[" << value_line << "]" << std::endl;
+				it->second = value_line;
+			}
+			else
+			{
+				m_param_value_map.emplace(param_line, value_line);
+			}
+		}
+	}
 }
--- src/ParamFile.h
+++ src/ParamFile.h
@@ -14,91 +14,91 @@
  */
 class ParamReader {
 public:
-    /**
-     * Constructor which will open up the parameter files specified and read
-     * each parameter-value pair into a map.
-     *
-     * Duplicate parameter resolution is determined by the file they came from:
-     *
-     * admin_file >> preparam_file >> param_file
-     *
-     * Each parameter-value pair can be extracted out by one of the extract_*()
-     * functions by passing a storage variable of the type expected.
-     *
-     * @note: The parameter file names passed into this function can be empty.
-     */
-    ParamReader(std::string const& param_file, std::string const& preparam_file, std::string const& admin_file);
+	/**
+	 * Constructor which will open up the parameter files specified and read
+	 * each parameter-value pair into a map.
+	 *
+	 * Duplicate parameter resolution is determined by the file they came from:
+	 *
+	 * admin_file >> preparam_file >> param_file
+	 *
+	 * Each parameter-value pair can be extracted out by one of the extract_*()
+	 * functions by passing a storage variable of the type expected.
+	 *
+	 * @note: The parameter file names passed into this function can be empty.
+	 */
+	ParamReader(std::string const& param_file, std::string const& preparam_file, std::string const& admin_file);
 
-    /**
-     * Extract a single numeric value or assign the default to `output`.
-     */
-    template<typename T>
-    bool extract(std::string const& param, T& output, T default_value);
+	/**
+	 * Extract a single numeric value or assign the default to `output`.
+	 */
+	template<typename T>
+	bool extract(std::string const& param, T& output, T default_value);
 
-    /**
-     * Extract and assign a single numeric value to `output` or exit the program.
-     */
-    template<typename T>
-    void extract_or_exit(std::string const& param, T& output);
+	/**
+	 * Extract and assign a single numeric value to `output` or exit the program.
+	 */
+	template<typename T>
+	void extract_or_exit(std::string const& param, T& output);
 
-    /**
-     * Extract multiple numeric values to `N` numbers in `output`, return false otherwise. 
-     */
-    template<typename T>
-    bool extract_multiple_no_default(std::string const& param, T* output, std::size_t N);
+	/**
+	 * Extract multiple numeric values to `N` numbers in `output`, return false otherwise.
+	 */
+	template<typename T>
+	bool extract_multiple_no_default(std::string const& param, T* output, std::size_t N);
 
-    /**
-     * Extract and assign multiple numeric values to `N` numbers in `output` or exit the program.
-     */
-    template<typename T>
-    void extract_multiple_or_exit(std::string const& param, T* output, std::size_t N);
+	/**
+	 * Extract and assign multiple numeric values to `N` numbers in `output` or exit the program.
+	 */
+	template<typename T>
+	void extract_multiple_or_exit(std::string const& param, T* output, std::size_t N);
 
-    /**
-     * Extract multiple numeric values or assign the default to `N` numbers in `output`.
-     */
-    template<typename T>
-    bool extract_multiple(std::string const& param, T* output, std::size_t N, T default_value);
+	/**
+	 * Extract multiple numeric values or assign the default to `N` numbers in `output`.
+	 */
+	template<typename T>
+	bool extract_multiple(std::string const& param, T* output, std::size_t N, T default_value);
 
-    /**
-     * Conditionally extract multiple numeric values or assign the default to `N` numbers in `output`.
-     */
-    template<typename T>
-    bool cond_extract_multiple(bool conditional, std::string const& param, T* output, std::size_t N, T default_value);
+	/**
+	 * Conditionally extract multiple numeric values or assign the default to `N` numbers in `output`.
+	 */
+	template<typename T>
+	bool cond_extract_multiple(bool conditional, std::string const& param, T* output, std::size_t N, T default_value);
 
-    /**
-     * Extract exactly `N` strings into `output`, return false otherwise. 
-     */
-    bool extract_multiple_strings_no_default(std::string const& param, std::vector<std::string>& output, std::size_t N);
+	/**
+	 * Extract exactly `N` strings into `output`, return false otherwise.
+	 */
+	bool extract_multiple_strings_no_default(std::string const& param, std::vector<std::string>& output, std::size_t N);
+	
+	/**
+	 * Extract exactly `N` strings and add them to `output` or exit the program. 
+	 */
+	void extract_multiple_strings_or_exit(std::string const& param, std::vector<std::string>& output, std::size_t N);
 
-    /**
-     * Extract exactly `N` strings and add them to `output` or exit the program. 
-     */
-    void extract_multiple_strings_or_exit(std::string const& param, std::vector<std::string>& output, std::size_t N);
-
-    /**
-     * Extract a matrix of strings with `num_cols` columns per row and add them to `output` or exit the program. 
-     */
-    void extract_string_matrix_or_exit(std::string const& param, std::vector<std::vector<std::string>>& output, std::size_t num_cols);
+	/**
+	 * Extract a matrix of strings with `num_cols` columns per row and add them to `output` or exit the program. 
+	 */
+	void extract_string_matrix_or_exit(std::string const& param, std::vector<std::vector<std::string>>& output, std::size_t num_cols);
 
 private:
-    /**
-     * Checks if the `param` specified exists in the `m_param_value_map`.
-     */
-    bool exists(std::string const& param);
+	/**
+	 * Checks if the `param` specified exists in the `m_param_value_map`.
+	 */
+	bool exists(std::string const& param);
 
-    /**
-     * Open and reads in argument-value pairs from `param_file` into `m_param_value_map`.
-     */
-    void parse_param_file(std::string const& param_file);
+	/**
+	 * Open and reads in argument-value pairs from `param_file` into `m_param_value_map`.
+	 */
+	void parse_param_file(std::string const& param_file);
 
-    /**
-     * Extracts the next value from `stream` into the output variable using the
-     * numeric limits of the `output` variable's type.
-     */
-    template<typename T>
-    bool raw_extract(std::istringstream& stream, T& output);
+	/**
+	 * Extracts the next value from `stream` into the output variable using the
+	 * numeric limits of the `output` variable's type.
+	 */
+	template<typename T>
+	bool raw_extract(std::istringstream& stream, T& output);
 
-    std::unordered_map<std::string, std::string> m_param_value_map;
+	std::unordered_map<std::string, std::string> m_param_value_map;
 };
 
 #endif // COVIDSIM_PARAM_FILE_HPP_INCLUDED_
