40ccc4cb466e51892af3f49ad437803449a537a9
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-09-01 17:42:57 +0100

    Unit tests for person infstat

--- src/Person.cpp
+++ src/Person.cpp
@@ -2,7 +2,8 @@
 #include "InfStat.h"
 
 
-bool Person::do_not_vaccinate() const {
+bool Person::do_not_vaccinate() const
+{
 
 	// Originally: inf < InfStat::InfectiousAlmostSymptomatic) || (inf >= InfStat::Dead_WasAsymp)
 	// ie, inf is either < -1, or >= 5. (ie, -2, -3, -5, or 5. There is no status for -4)
@@ -11,11 +12,13 @@ bool Person::do_not_vaccinate() const {
 	return this->is_dead() || this->is_case() || this->is_recovered_symp();
 }
 
-bool Person::is_alive() const {
+bool Person::is_alive() const
+{
 	return !this->is_dead();
 }
 
-bool Person::is_case() const {
+bool Person::is_case() const
+{
 	return (this->inf == InfStat::Case);
 }
 
@@ -26,23 +29,28 @@ bool Person::is_dead() const
 	return (this->inf == InfStat::Dead_WasSymp || this->inf == InfStat::Dead_WasAsymp);
 }
 
-bool Person::is_dead_was_symp() const {
+bool Person::is_dead_was_symp() const
+{
 	return this->inf == InfStat::Dead_WasSymp;
 }
 
-bool Person::is_dead_was_asymp() const {
+bool Person::is_dead_was_asymp() const
+{
 	return this->inf == InfStat::Dead_WasAsymp;
 }
 
-bool Person::is_immune_at_start() const {
+bool Person::is_immune_at_start() const
+{
 	return (this->inf == InfStat::ImmuneAtStart);
 }
 
-bool Person::is_infectious_almost_symptomatic() const {
+bool Person::is_infectious_almost_symptomatic() const
+{
 	return this->inf == InfStat::InfectiousAlmostSymptomatic;
 }
 
-bool Person::is_infectious_asymptomatic_not_case() const {
+bool Person::is_infectious_asymptomatic_not_case() const
+{
 	return this->inf == InfStat::InfectiousAsymptomaticNotCase;
 }
 
@@ -60,7 +68,8 @@ bool Person::is_never_symptomatic() const
 			(this->inf == InfStat::Dead_WasAsymp);
 }
 
-bool Person::is_not_yet_symptomatic() const {
+bool Person::is_not_yet_symptomatic() const
+{
 	return (this->inf == InfStat::Susceptible ||
 			this->inf == InfStat::Latent ||
 			this->inf == InfStat::InfectiousAlmostSymptomatic);
@@ -72,11 +81,13 @@ bool Person::is_recovered() const
 	return (this->inf == InfStat::RecoveredFromSymp) || (this->inf == InfStat::RecoveredFromAsymp);
 }
 
-bool Person::is_recovered_symp() const {
+bool Person::is_recovered_symp() const
+{
 	return this->inf == InfStat::RecoveredFromSymp;
 }
 
-bool Person::is_susceptible() const {
+bool Person::is_susceptible() const
+{
 	return (this->inf == InfStat::Susceptible);
 }
 
@@ -92,7 +103,8 @@ bool Person::is_susceptible_or_infected() const
 
 /********************************************************/
 
-void Person::set_case() {
+void Person::set_case()
+{
 	this->inf = InfStat::Case;
 }
 
@@ -108,23 +120,28 @@ void Person::set_dead() {
 	this->inf = (this->inf == InfStat::Case) ? InfStat::Dead_WasSymp : InfStat::Dead_WasAsymp;
 }
 
-void Person::set_immune_at_start() {
+void Person::set_immune_at_start()
+{
 	this->inf = InfStat::ImmuneAtStart;
 }
 
-void Person::set_infectious_almost_symptomatic() {
+void Person::set_infectious_almost_symptomatic()
+{
 	this->inf = InfStat::InfectiousAlmostSymptomatic;
 }
 
-void Person::set_infectious_asymptomatic_not_case() {
+void Person::set_infectious_asymptomatic_not_case()
+{
 	this->inf = InfStat::InfectiousAsymptomaticNotCase;
 }
 
-void Person::set_latent() {
+void Person::set_latent()
+{
 	this->inf = InfStat::Latent;
 }
 
-void Person::set_recovered() {
+void Person::set_recovered()
+{
 
 	/*	Similar to deaths, this used to be: inf = (InfStat)(InfStat::Recovered * inf / abs(inf));
 		Where inf / abs(inf) becomes +/- 1. So resulting state has same sign as incoming state. 
@@ -136,6 +153,7 @@ void Person::set_recovered() {
 	this->inf = (this->inf == InfStat::Case) ? InfStat::RecoveredFromSymp : InfStat::RecoveredFromAsymp;
 }
 
-void Person::set_susceptible() {
+void Person::set_susceptible()
+{
 	this->inf = InfStat::Susceptible;
 }
--- unit_tests/test-person.cpp
+++ unit_tests/test-person.cpp
@@ -1,6 +1,207 @@
 #include <gtest/gtest.h>
 
-TEST(CovidSimBasicTest, BasicTest)
+#include "Models/Person.h"
+
+TEST(CovidSimPersonTests, Person_Susceptible)
 {
-    ASSERT_EQ(1, 1);
+  Person* P = new Person();
+  
+  P->set_susceptible();
+  ASSERT_FALSE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_FALSE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_TRUE(P->is_not_yet_symptomatic());
+  ASSERT_TRUE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Latent)
+{
+  Person* P = new Person();
+  P->set_latent();
+  ASSERT_FALSE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_TRUE(P->is_latent());
+  ASSERT_TRUE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_TRUE(P->is_not_yet_symptomatic());
+  ASSERT_TRUE(P->is_susceptible_or_infected());
+
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Inf_Alm_Sympt)
+{
+  Person* P = new Person();
+  P->set_infectious_almost_symptomatic();
+  ASSERT_FALSE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_TRUE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_FALSE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_TRUE(P->is_not_yet_symptomatic());
+  ASSERT_TRUE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Case)
+{
+  Person* P = new Person();
+  P->set_case();
+  ASSERT_TRUE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_TRUE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_FALSE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_TRUE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Inf_Asymp_Not_Case)
+{
+  Person* P = new Person();
+  P->set_infectious_asymptomatic_not_case();
+  ASSERT_FALSE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_TRUE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_TRUE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_TRUE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Death_Symp)
+{
+  Person* P = new Person();
+  P->set_case();
+  P->set_dead();
+  ASSERT_TRUE(P->do_not_vaccinate());
+  ASSERT_TRUE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_TRUE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_FALSE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_FALSE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_FALSE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Death_ASymp)
+{
+  Person* P = new Person();
+  P->set_susceptible();
+  P->set_dead();
+  ASSERT_TRUE(P->do_not_vaccinate());
+  ASSERT_TRUE(P->is_dead());
+  ASSERT_TRUE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_TRUE(P->is_never_symptomatic());
+  ASSERT_FALSE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_FALSE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_FALSE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Recover_Symp)
+{
+  Person* P = new Person();
+  P->set_case();
+  P->set_recovered();
+  ASSERT_TRUE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_FALSE(P->is_never_symptomatic());
+  ASSERT_TRUE(P->is_recovered());
+  ASSERT_TRUE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_FALSE(P->is_susceptible_or_infected());
+  delete P;
+}
+
+TEST(CovidSimPersonTests, Person_Recover_ASymp)
+{
+  Person* P = new Person();
+  P->set_susceptible();
+  P->set_recovered();
+  ASSERT_FALSE(P->do_not_vaccinate());
+  ASSERT_FALSE(P->is_dead());
+  ASSERT_FALSE(P->is_dead_was_asymp());
+  ASSERT_FALSE(P->is_dead_was_symp());
+  ASSERT_FALSE(P->is_case());
+  ASSERT_FALSE(P->is_immune_at_start());
+  ASSERT_FALSE(P->is_infectious_almost_symptomatic());
+  ASSERT_FALSE(P->is_infectious_asymptomatic_not_case());
+  ASSERT_FALSE(P->is_latent());
+  ASSERT_TRUE(P->is_never_symptomatic());
+  ASSERT_TRUE(P->is_recovered());
+  ASSERT_FALSE(P->is_recovered_symp());
+  ASSERT_TRUE(P->is_alive());
+  ASSERT_FALSE(P->is_not_yet_symptomatic());
+  ASSERT_FALSE(P->is_susceptible_or_infected());
+  delete P;
 }
