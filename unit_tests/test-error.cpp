342196d883a0359492c80a1b41f398d511534507
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-16 20:03:26 +0000

    Works on linux - try \] on win

--- unit_tests/test-error.cpp
+++ unit_tests/test-error.cpp
@@ -4,13 +4,13 @@
 TEST(ErrorDeathTest, ErrorCritical) {
   ASSERT_DEATH({
     ErrorCritical("Test Error", "test-error.cpp", 7);
-  }, "test-error.cpp line 7] Test Error");
+  }, "test-error.cpp line 7\] Test Error");
 }
 
 TEST(ErrorDeathTest, err_critical_fmt) {
   ASSERT_DEATH({
     ERR_CRITICAL_FMT("Test %s\n", "TEST_STRING");
-  }, "test-error.cpp line 12] Test TEST_STRING");
+  }, "test-error.cpp line 12\] Test TEST_STRING");
 }
 
 TEST(ErrorDeathTest, err_critical) {
