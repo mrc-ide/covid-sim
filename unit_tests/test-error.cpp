d9f95949490ad4273179e92bbd13aeb280bf959c
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-16 18:20:14 +0000

    Try again

--- unit_tests/test-error.cpp
+++ unit_tests/test-error.cpp
@@ -4,13 +4,13 @@
 TEST(ErrorDeathTest, ErrorCritical) {
   ASSERT_DEATH({
     ErrorCritical("Test Error", "test-error.cpp", 7);
-  }, "\\[test-error.cpp line 7\\] Test Error");
+  }, "test-error.cpp line 7\\] Test Error");
 }
 
 TEST(ErrorDeathTest, err_critical_fmt) {
   ASSERT_DEATH({
     ERR_CRITICAL_FMT("Test ERR_CRITICAL_FMT", "test-error.cpp", 12);
-  }, "\\[test-error.cpp line 12\\] Test ERR_CRITICAL_FMT");
+  }, "test-error.cpp line 12\\] Test ERR_CRITICAL_FMT");
 }
 
 TEST(ErrorDeathTest, err_critical) {
