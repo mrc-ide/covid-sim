9fb5d02e9445f9da1fb6e671a43fb580252e17d5
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-16 20:10:41 +0000

    grr

--- unit_tests/test-error.cpp
+++ unit_tests/test-error.cpp
@@ -4,17 +4,17 @@
 TEST(ErrorDeathTest, ErrorCritical) {
   ASSERT_DEATH({
     ErrorCritical("Test Error", "test-error.cpp", 7);
-  }, "test-error.cpp line \d+\] Test Error");
+  }, ".*test-error.cpp line.*Test Error.*");
 }
 
 TEST(ErrorDeathTest, err_critical_fmt) {
   ASSERT_DEATH({
     ERR_CRITICAL_FMT("Test %s\n", "TEST_STRING");
-  }, "test-error.cpp line \d+\] Test TEST_STRING");
+  }, ".*test-error.cpp line.*Test TEST_STRING.*");
 }
 
 TEST(ErrorDeathTest, err_critical) {
   ASSERT_DEATH({
     ERR_CRITICAL("Test ERR_CRITICAL");
-  }, "Test ERR_CRITICAL");
+  }, ".*Test ERR_CRITICAL.*");
 }
