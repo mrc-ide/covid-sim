a746a49dcf384588ecd3d32db188d5bbd5b21deb
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-16 15:40:39 +0000

    Test ERR_CRITICAL and ERR_CRITICAL_FMT

--- unit_tests/test-error.cpp
+++ unit_tests/test-error.cpp
@@ -6,5 +6,12 @@ TEST(Error, Error)
   ASSERT_DEATH({
     ErrorCritical("Test Error", "test-error.cpp", 7);
   }, "[Test Error line 7]");
-}
 
+  ASSERT_DEATH({
+    ERR_CRITICAM_FTM("Test ERR_CRITICAL_FMT", "test-error.cpp", 11);
+  }, "[Test ERR_CRITICAL_FMT line 11]");
+
+  ASSERT_DEATH({
+    ERR_CRITICAL("Test ERR_CRITICAL");
+  }, "Test ERR_CRITICAL");
+}
