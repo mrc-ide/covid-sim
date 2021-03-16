ae12aef85c2ae9c5f6332d22d0cbf30fc680c76c
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-16 17:12:45 +0000

    Fix [ match on win

--- unit_tests/test-error.cpp
+++ unit_tests/test-error.cpp
@@ -1,16 +1,19 @@
 #include <gtest/gtest.h>
 #include "Error.h"
 
-TEST(Error, Error)
-{
+TEST(ErrorDeathTest, ErrorCritical) {
   ASSERT_DEATH({
     ErrorCritical("Test Error", "test-error.cpp", 7);
-  }, "[Test Error line 7]");
+  }, "\\[Test Error line 7\\]");
+}
 
+TEST(ErrorDeathTest, err_critical_fmt) {
   ASSERT_DEATH({
-    ERR_CRITICAM_FTM("Test ERR_CRITICAL_FMT", "test-error.cpp", 11);
-  }, "[Test ERR_CRITICAL_FMT line 11]");
+    ERR_CRITICAL_FMT("Test ERR_CRITICAL_FMT", "test-error.cpp", 12);
+  }, "\\[Test ERR_CRITICAL_FMT line 12\\]");
+}
 
+TEST(ErrorDeathTest, err_critical) {
   ASSERT_DEATH({
     ERR_CRITICAL("Test ERR_CRITICAL");
   }, "Test ERR_CRITICAL");
