b8ed65df46efe6b05bb3c280ee8e385b80788d1d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-17 18:33:35 +0000

    First params unit tests

---
+++ unit_tests/test-params.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "Files.h"
+#include "ReadParams.h"
+
+
+ParamMap test_map(str::string contents) {
+  Files::xfopen("test_param_tmp.txt", "w");
+  Files::fprintf("%s", contents);
+  Files::xfclose();
+  ParamMap P = Params::read_params_map("test_param_tmp.txt");
+  Files::xremove("test_param_tmp.txt");
+  return P;
+}
+
+TEST(ReadParams, scalar_int) {
+  Param P;
+  ParamMap PM = test_map("[A]\n42[C]99");
+  EXPECT_EQ(Params::get_int(PM, PM, "A", 0, P), 42);    // Basic usage
+  EXPECT_EQ(Params::get_int(PM, PM, "C", 0, P), 99);    // End of file, no last line
+  EXPECT_EQ(Params::get_int(PM, PM, "B", 1, P), 1);\
+  EXPECT_EQ(Params::get_int(PM, PM, PM, "A", 0, P), 42);
+  EXPECT_EQ(Params::get_int(PM, PM, PM, "B", 1, P), 1);
+
+  EXPECT_EQ(Params::get_int_ff(false, PM, PM, "A", 0, P), 42);
+  EXPECT_EQ(Params::get_int_ff(true, PM, PM, "A", 37, P), 37);
+  EXPECT_EQ(Params::get_req(PM, PM, PM, "A", 0, P), 42);
+  EXPECT_EQ(Params::get_req(PM, PM, "A", 0, P), 42);
+}
