50f016180bc83eed9eba02bd1101114ff26da9d7
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-03-17 12:25:08 +0000

    Fix file test

--- unit_tests/test-files.cpp
+++ unit_tests/test-files.cpp
@@ -6,8 +6,8 @@
 TEST(Files, BasicTests) {
   uint64_t* buf = new uint64_t[10];
   uint64_t* compare = new uint64_t[10];
-  for (int i=0; i<10; i++) {
-    buf[i] = 2e30 + i;
+  for (uint64_t i=0; i<10; i++) {
+    buf[i] = buf[i] = 18000000000000000000L + i;
   }
 
   FILE* f = Files::xfopen("test_basictest.txt", "wb");
@@ -15,13 +15,13 @@ TEST(Files, BasicTests) {
   Files::xfclose(f);
 
   Files::xrename("test_basictest.txt", "renamed_test_basictest.txt");
-  FILE* f2 = Files::xfopen("renamed_test_basictest.txt", "wb");
+  FILE* f2 = Files::xfopen("renamed_test_basictest.txt", "rb");
   Files::fread_big(compare, sizeof(uint64_t), 10, f2);
   Files::xfclose(f2);
   Files::xremove("renamed_test_basictest.txt");
 
   int matches = 0;
-  for (int i=0; i<10; i++) {
+  for (int i = 0; i < 10; i++) {
     matches += (buf[i] == compare[i]) ? 1 : 0;
   }
 
