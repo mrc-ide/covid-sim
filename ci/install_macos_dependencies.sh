299378abc9b870bf8fd43d1818fa76f9f3824e3d
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-02-02 14:55:35 +0000

    Attempt to fix MacOS CI

--- ci/install_macos_dependencies.sh
+++ ci/install_macos_dependencies.sh
@@ -2,3 +2,5 @@
 
 brew install llvm cmake libomp python3 coreutils
 export CMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -Xpreprocessor -fopenmp"
+export OpenMP_C_LIB_NAMES="libomp"
+export OpenMP_C_FLAGS="-fopenmp"
